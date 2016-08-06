989ed89910efa1068c97482adf25aa7e9af636bc
Grig <yeghiazaryan.grigor@iamcloud.com>
2016-08-07 00:29:06 +0400

    first chekin

---
+++ WebPortal/SPA/Controller/CdFiltersController.js
@@ -0,0 +1,16 @@
+﻿var CdFiltersController = function ($scope) {
+    $scope.models = {
+        filters: [
+                        { id: "1", filter: "All",dataType= },
+                        { id: "2", filter: "color-1" },
+                        { id: "3", filter: "color-2" }
+        ]
+    };
+    $scope.selectedFilter = $scope.models.filters[0];
+
+    $scope.changeFilter = function (f) {
+        $scope.selectedFilter = f;
+    }
+}
+
+CdFiltersController.$inject = ['$scope'];
