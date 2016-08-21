7db05698b027e57c2c2a50571c443db078471954
Vachik Yeghiazaryan <vyeghiazaryan@outlook.com>
2016-08-22 00:14:13 +0400

    add-to-cart-interaction functionality

--- WebPortal/js/main.js
+++ WebPortal/js/main.js
@@ -319,6 +319,7 @@ jQuery(document).ready(function ($) {
 
                 var title = itemInfo.find('.title');
                 var description = itemInfo.find('.description');
+                var addToCartBtn = itemInfo.find('.cd-add-to-cart');
 
                 var currProd = '';
                 productId = parseInt(productId);
@@ -331,6 +332,8 @@ jQuery(document).ready(function ($) {
                     }
                     title.text(currProd.Name);
                     description.text(currProd.Description);
+                    addToCartBtn.data("product-id", productId);
+                    addToCartBtn.data("price", currProd.Price);
 
                     //if first time bind pictures
                     if ($(slider.children()[0]).find('img').length <= 1) {
@@ -562,3 +565,242 @@ jQuery.fn.putCursorAtEnd = function () {
         }
     });
 };
+
+
+
+/*****************************************************
+	add-to-cart-interaction functionality
+*****************************************************/
+
+
+jQuery(document).ready(function ($) {
+    var cartWrapper = $('.cd-cart-container');
+    //product id - you don't need a counter in your real project but you can use your real product id
+    var productId = 0;
+    var currentProduct = '';
+
+    if (cartWrapper.length > 0) {
+        //store jQuery objects
+        var cartBody = cartWrapper.find('.body');
+        var cartList = cartBody.find('ul').eq(0);
+        var cartTotal = cartWrapper.find('.checkout').find('span');
+        var cartTrigger = cartWrapper.children('.cd-cart-trigger');
+        var cartCount = cartTrigger.children('.count')
+        var addToCartBtn = $('.cd-add-to-cart');
+        var undo = cartWrapper.find('.undo');
+        var undoTimeoutId;
+
+        //add product to cart
+        var locker = true;
+        addToCartBtn.on('click', function (event) {
+            event.preventDefault();
+            var $this = $(this);
+
+            $this.css('visibility', 'hidden');
+            setTimeout(function () {
+                $this.css('visibility', 'visible');
+                if (true) {
+                    locker = false;
+                    addToCart($this);
+                    locker = true;
+                }
+            }, 100);
+
+        });
+
+        //open/close cart
+        cartTrigger.on('click', function (event) {
+            event.preventDefault();
+            toggleCart();
+        });
+
+        //close cart when clicking on the .cd-cart-container::before (bg layer)
+        cartWrapper.on('click', function (event) {
+            if ($(event.target).is($(this))) toggleCart(true);
+        });
+
+        //delete an item from the cart
+        cartList.on('click', '.delete-item', function (event) {
+            event.preventDefault();
+            removeProduct($(event.target).parents('.product'));
+        });
+
+        //update item quantity
+        cartList.on('change', 'select', function (event) {
+            quickUpdateCart();
+        });
+
+        //reinsert item deleted from the cart
+        undo.on('click', 'a', function (event) {
+            clearInterval(undoTimeoutId);
+            event.preventDefault();
+            cartList.find('.deleted').addClass('undo-deleted').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function () {
+                $(this).off('webkitAnimationEnd oanimationend msAnimationEnd animationend').removeClass('deleted undo-deleted').removeAttr('style');
+                quickUpdateCart();
+            });
+            undo.removeClass('visible');
+        });
+    }
+
+    function toggleCart(bool) {
+        var cartIsOpen = (typeof bool === 'undefined') ? cartWrapper.hasClass('cart-open') : bool;
+
+        if (cartIsOpen) {
+            cartWrapper.removeClass('cart-open');
+            //reset undo
+            clearInterval(undoTimeoutId);
+            undo.removeClass('visible');
+            cartList.find('.deleted').remove();
+
+            setTimeout(function () {
+                cartBody.scrollTop(0);
+                //check if cart empty to hide it
+                if (Number(cartCount.find('li').eq(0).text()) == 0) cartWrapper.addClass('empty');
+            }, 500);
+        } else {
+            cartWrapper.addClass('cart-open');
+        }
+    }
+
+    function addToCart(trigger) {
+        var cartIsEmpty = cartWrapper.hasClass('empty');
+        //get productId and currentProduct from addToCartBtn
+        getProduct();
+        //update cart product list
+        addProduct();
+        //update number of items 
+        updateCartCount(cartIsEmpty);
+        //update total price
+        updateCartTotal(trigger.data('price'), true);
+        //show cart
+        cartWrapper.removeClass('empty');
+    }
+    function getProduct() {
+        productId = parseInt(addToCartBtn.data('product-id'));
+        //currentProduct = $.grep(Products, function (e) { return e.id == productId; });
+        if (productId == currentProduct.Id) {
+            return;
+        }
+        for (var i = 0; i < Products.length; i++) {
+            if (Products[i].Id == productId) {
+                currentProduct = Products[i];
+                break;
+            }
+        }
+
+        if (currentProduct.length == 0 || currentProduct.length > 1) {
+            console.log('logic error:   '
+                + 'currentProduct.length == 0 || currentProduct.length > 1 currentProduct.length = '
+                + currentProduct.length);
+        }
+    }
+
+    function addProduct() {
+        //this is just a product placeholder
+        //you should insert an item with the selected product info
+        //replace productId, productName, price and url with your real product info
+        if (currentProduct != null) {
+            var $productsInCart = cartList.children();
+
+            var $prodQty = $productsInCart.find('select#cd-product-' + currentProduct.Id);
+
+            if ($prodQty && $prodQty.length > 0) {
+                // increment existing product quantity
+                var quantity = parseInt($prodQty.val());
+                if (quantity < 9) {
+                    $prodQty.val((quantity + 1).toString());
+                }
+                else {
+                    //alert("You can't order more than 9 piece of a product");
+                }
+            } else {
+                //add new product to cart
+                var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="'
+                    + currentProduct.ImageUrls[0] + '" alt="' + currentProduct.Name
+                    + '"></a></div><div class="product-details"><h3><a href="#0">'
+                    + currentProduct.Name + '</a></h3><span class="price">$'
+                    + currentProduct.Price +
+                    '</span><div class="actions"><a href="#0" class="delete-item">Delete</a><div class="quantity"><label for="cd-product-'
+                    + productId + '">Qty</label><span class="select"><select id="cd-product-'
+                    + productId + '" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
+                cartList.prepend(productAdded);
+            }
+        }
+    }
+
+    function removeProduct(product) {
+        clearInterval(undoTimeoutId);
+        cartList.find('.deleted').remove();
+
+        var topPosition = product.offset().top - cartBody.children('ul').offset().top,
+			productQuantity = Number(product.find('.quantity').find('select').val()),
+			productTotPrice = Number(product.find('.price').text().replace('$', '')) * productQuantity;
+
+        product.css('top', topPosition + 'px').addClass('deleted');
+
+        //update items count + total price
+        updateCartTotal(productTotPrice, false);
+        updateCartCount(true, -productQuantity);
+        undo.addClass('visible');
+
+        //wait 8sec before completely remove the item
+        undoTimeoutId = setTimeout(function () {
+            undo.removeClass('visible');
+            cartList.find('.deleted').remove();
+        }, 8000);
+    }
+
+    function quickUpdateCart() {
+        var quantity = 0;
+        var price = 0;
+
+        cartList.children('li:not(.deleted)').each(function () {
+            var singleQuantity = Number($(this).find('select').val());
+            quantity = quantity + singleQuantity;
+            price = price + singleQuantity * Number($(this).find('.price').text().replace('$', ''));
+        });
+
+        cartTotal.text(price.toFixed(2));
+        cartCount.find('li').eq(0).text(quantity);
+        cartCount.find('li').eq(1).text(quantity + 1);
+
+
+        $('.cd-add-to-cart').css('visibility', 'visible');
+    }
+
+    function updateCartCount(emptyCart, quantity) {
+        if (typeof quantity === 'undefined') {
+            var actual = Number(cartCount.find('li').eq(0).text()) + 1;
+            var next = actual + 1;
+
+            if (emptyCart) {
+                cartCount.find('li').eq(0).text(actual);
+                cartCount.find('li').eq(1).text(next);
+            } else {
+                cartCount.addClass('update-count');
+
+                setTimeout(function () {
+                    cartCount.find('li').eq(0).text(actual);
+                }, 150);
+
+                setTimeout(function () {
+                    cartCount.removeClass('update-count');
+                }, 200);
+
+                setTimeout(function () {
+                    cartCount.find('li').eq(1).text(next);
+                }, 230);
+            }
+        } else {
+            var actual = Number(cartCount.find('li').eq(0).text()) + quantity;
+            var next = actual + 1;
+
+            cartCount.find('li').eq(0).text(actual);
+            cartCount.find('li').eq(1).text(next);
+        }
+    }
+
+    function updateCartTotal(price, bool) {
+        bool ? cartTotal.text((Number(cartTotal.text()) + Number(price)).toFixed(2)) : cartTotal.text((Number(cartTotal.text()) - Number(price)).toFixed(2));
+    }
+});
